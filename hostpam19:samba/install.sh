cc8bf6cbdcc366b34994903d7df91c2de35bedb0
Adrian <anarvaez1599@gmail.com>
2019-12-04 13:53:53 +0100

    pam

--- hostpam19:samba/install.sh
+++ hostpam19:samba/install.sh
@@ -6,8 +6,12 @@ useradd local3
 echo "local1" | passwd --stdin local1 
 echo "local2" | passwd --stdin local2
 echo "local3" | passwd --stdin local3
+
+bash /opt/docker/auth.sh
+cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 cp /opt/docker/login.defs /etc/login.defs
 cp /opt/docker/system-auth /etc/pam.d/system-auth
 cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+ln -sf /etc/pam.d/system-auth-edt /etc/pam.d/system-auth
