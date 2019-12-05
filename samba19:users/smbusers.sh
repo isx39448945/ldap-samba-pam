97e00d175e5bc66a014e1b8317e1034b77b5b773
Adrian <anarvaez1599@gmail.com>
2019-12-05 10:19:59 +0100

    insercio pam

---
+++ samba19:users/smbusers.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# creació dels usuaris
+
+#users unix locals
+usersunix="local1 local2 local3"
+for userunix in $usersunix
+do
+  useradd $userunix
+  echo -e "$userunix\n$userunix" | passwd -a $userunix
+done
+
+#users samba locals
+userssamba="lila roc pla patipla"
+for usersamba in $userssamba
+do
+  useradd $usersamba
+  echo -e "$usersamba\n$usersamba" | smbpasswd -a $usersamba
+done
+
+#users ldap
+usersldap="pere marta anna pau jordi"
+for userldap in $usersldap
+do
+  echo -e "$userldap\n$userldap" | smbpasswd -a $userldap
+done  
